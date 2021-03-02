ce0d996267eb12f129677d92b14a4181f970e328
Karol Krizka <kkrizka@gmail.com>
2021-03-02 22:36:47 +0000

    Automatic discovery of new devices.

--- run.py
+++ run.py
@@ -6,7 +6,7 @@ import time
 import re
 import dbus
 import dbus.mainloop.glib
-from gi.repository import GObject as gobject
+from gi.repository import GLib
 from influxdb import InfluxDBClient
 
 #
@@ -16,7 +16,7 @@ config=json.load(open('config.json'))
 #
 # Initialize DBus
 dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
-mainloop = gobject.MainLoop()
+mainloop = GLib.MainLoop()
 bus=dbus.SystemBus()
 
 #
@@ -75,45 +75,75 @@ class GATTMonitor:
         point['fields']['value']=value
         self.dbclient.write_points([point])
 
-#
-# Find all available variables
-characteristics=[]
-mngr=bus.get_object('org.bluez','/')
-re_char=re.compile('/org/bluez/hci0/dev_[0-9A-Z_]+/service[0-9a-z]+/char[0-9a-z]+$')
-for obj in filter(lambda obj: re_char.match(obj), mngr.GetManagedObjects(dbus_interface='org.freedesktop.DBus.ObjectManager')):
-    obj=bus.get_object('org.bluez', obj)
+class CharacteristicManager:
+    def __init__(self, bus):
+        self.bus=bus
+
+        self.characteristics={}
+
+        self.re_char=re.compile('/org/bluez/hci0/dev_[0-9A-Z_]+/service[0-9a-z]+/char[0-9a-z]+$')
+
+        #
+        # Find services
+        mngr=bus.get_object('org.bluez','/')
+
+        # Add existing services
+        for obj in filter(lambda obj: self.re_char.match(obj),mngr.GetManagedObjects(dbus_interface='org.freedesktop.DBus.ObjectManager')):
+            self.add_monitor(obj)
+            
+        # Monitor for new services
+        mngr.connect_to_signal("InterfacesAdded"  , self.InterfacesAdded)
+
+    def InterfacesAdded(self, path, interfaces):
+        if not self.re_char.match(path): return
+        self.add_monitor(path)
+
+    def add_monitor(self, path):
+        if path in characteristics:
+            print(f'Already monitoring {path}')
+            return
+
+        print(f'Adding characteristic at {path}')
+        obj=self.bus.get_object('org.bluez', path)
     
-    # Check if this is a known characteristic
-    UUID=obj.Get('org.bluez.GattCharacteristic1', 'UUID', dbus_interface='org.freedesktop.DBus.Properties')
-    filt=filter(lambda char: char['uuid']==UUID, known_characteristics)
-    try:
-        info=next(filt).copy()
-    except StopIteration:
-        continue
+        # Check if this is a known characteristic
+        UUID=obj.Get('org.bluez.GattCharacteristic1', 'UUID', dbus_interface='org.freedesktop.DBus.Properties')
+        filt=filter(lambda char: char['uuid']==UUID, known_characteristics)
+        try:
+            info=next(filt).copy()
+        except StopIteration:
+            return
 
-    # Build up information about this characteristic
-    info['obj']=obj
+        # Build up information about this characteristic
+        info['obj']=obj
 
-    Service=obj.Get('org.bluez.GattCharacteristic1', 'Service', dbus_interface='org.freedesktop.DBus.Properties')
-    Service=bus.get_object('org.bluez', Service)
-    info['Service']=Service
-    info['ServiceUUID']=Service.Get('org.bluez.GattService1','UUID',dbus_interface='org.freedesktop.DBus.Properties')
+        Service=obj.Get('org.bluez.GattCharacteristic1', 'Service', dbus_interface='org.freedesktop.DBus.Properties')
+        Service=self.bus.get_object('org.bluez', Service)
+        info['Service']=Service
+        info['ServiceUUID']=Service.Get('org.bluez.GattService1','UUID',dbus_interface='org.freedesktop.DBus.Properties')
     
-    Device=Service.Get('org.bluez.GattService1', 'Device', dbus_interface='org.freedesktop.DBus.Properties')
-    Device=bus.get_object('org.bluez', Device)
-    info['Device']=Device
+        Device=Service.Get('org.bluez.GattService1', 'Device', dbus_interface='org.freedesktop.DBus.Properties')
+        Device=self.bus.get_object('org.bluez', Device)
+        info['Device']=Device
 
-    # Determine device information
-    info['DeviceName']=Device.Get('org.bluez.Device1','Name'   ,dbus_interface='org.freedesktop.DBus.Properties')
-    info['DeviceMAC' ]=Device.Get('org.bluez.Device1','Address',dbus_interface='org.freedesktop.DBus.Properties')
+        # Determine device information
+        info['DeviceName']=Device.Get('org.bluez.Device1','Name'   ,dbus_interface='org.freedesktop.DBus.Properties')
+        info['DeviceMAC' ]=Device.Get('org.bluez.Device1','Address',dbus_interface='org.freedesktop.DBus.Properties')
 
-    characteristic=GATTMonitor(dbclient, obj,
+        characteristic=GATTMonitor(dbclient, obj,
                                    info['uuid'], info['name'], info['type'],
                                    info['ServiceUUID'],
                                    info['DeviceName'], info['DeviceMAC'])
-print('---')
+        self.characteristics[path]=characteristic
+
+        return characteristic
+        
+#
+# Find all available variables
+characteristics=[]
+mngr=CharacteristicManager(bus)
 
 #
 # Monitoring loop
-
+print('Starting main loop')
 mainloop.run()
